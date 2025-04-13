19e66fd4aeceeb94dca4b640bd7f3c8a34f5ef2c
Sarvagya196 <sarvagya.acharya@alchemytech.ca>
2025-04-13 20:01:47 +0530

    database typo fix

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/chamdra-app', {
+        await mongoose.connect('mongodb://localhost:27017/chandra-app', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
