c96bde9dedc18b2a2b386305cb9ca28023c828ae
Sarvagya196 <sarvagya.acharya@alchemytech.ca>
2025-04-18 20:01:50 +0530

    get users endpoints update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,21 @@
 const service = require('../services/user.service');
 
 exports.getUsers = async (req, res) => {
-    const users = await service.getUsers();
-    res.json(users);
+    try{
+        const users = await service.getUsers();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+    
+};
+
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await service.getUserById(req.params.id);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
--- services/user.service.js
+++ services/user.service.js
@@ -1,3 +1,10 @@
 const repo = require('../repositories/user.repo');
 
-exports.getUsers = () => repo.getAllUsers();
+exports.getUsers = async () => {
+    return await repo.find({}, {Id: 1, Name: 1, _id:0})
+    // repo.getAllUsers();
+}
+
+exports.getUserById = async (id) => {
+    return await repo.findOne({ Id: id });
+};
