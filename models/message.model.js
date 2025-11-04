1b891a3aecce0c4fb1e3b1304cab377c4f7508e4
Hrishitj <hrishitj@gmail.com>
2025-11-04 23:55:43 +0530

    added parent message id for reply msgs

--- models/message.model.js
+++ models/message.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
   enquiryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Enquiry', required: true },
   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  parentMessageId: { type: mongoose.Schema.Types.ObjectId, ref: 'Message', default: null },
   message: String,
   messageType: { type: String, enum: ['text','video','image','file'], default: 'text' },
   mediaKey: String,
